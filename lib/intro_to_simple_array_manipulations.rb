030148873bfc4b8f782052dbebe4311908df29d8
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-15 13:49:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,10 +6,14 @@ def using_unshift(array,string)
   array.unshift(string)
 end 
 
-def using_pop(string)
-  array.pop(string)
+def using_pop(array)
+  array.pop
 end
 
-def using_shift(string)
-  array.shift(string)
+def using_shift(array)
+  array.shift
 end 
+
+def pop_with_args(array)
+  array.pop(2)
+end
